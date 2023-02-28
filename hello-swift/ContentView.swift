ab79c6fa4c6791e44118b2c9f8fa3fb4d8bb8999
Lucas Davi <lucasdavimarinho9@gmail.com>
2023-02-28 14:57:59 -0300

    ...

--- hello-swift/ContentView.swift
+++ hello-swift/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "star")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.purple)
             Text("Hello, world!")
+                .foregroundColor(Color.green)
         }
         .padding()
     }
