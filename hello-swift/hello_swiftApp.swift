8b7be1737b96b42db6716825253f68bd785f52ff
Lucas Davi <lucasdavimarinho9@gmail.com>
2023-02-28 14:56:06 -0300

    Initial Commit

---
+++ hello-swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello-swift
+//
+//  Created by user on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
